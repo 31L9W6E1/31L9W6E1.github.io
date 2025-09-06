d68692d550cd8d89317e5cb8783265f7a0e291c7
Hekkk1 <72281268+Hekkk1@users.noreply.github.com>
2025-09-06 20:14:20 +0800

    version --1.02

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // Do NOT set basePath or assetPrefix for a user/organization site or custom domain.
 };
 
 export default nextConfig;
