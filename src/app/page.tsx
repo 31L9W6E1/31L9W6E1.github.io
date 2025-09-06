a5894887163e9fe71507c62fcad5d2605ebd2424
Hekkk1 <72281268+Hekkk1@users.noreply.github.com>
2025-09-06 21:58:31 +0800

    version -1.03

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,12 +11,13 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { GlowingLineChart } from "@/components/ui/glowing-line";
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card";
-
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 
 export default function Home() {
-
-  
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_50px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[22px] row-start-2 items-center sm:items-start">
@@ -103,6 +104,14 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             Sign-up
+          </a>
+        </div>
+        <div>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://google.com"
+          >
+            hi
           </a>
         </div>
       </main>
