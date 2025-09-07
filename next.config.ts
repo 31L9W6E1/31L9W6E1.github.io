552274785bb1bc9f29b25a8570fc8be034eda25c
Hekkk1 <baqateacate@gmail.com>
2025-09-08 01:28:00 +0800

    version 2.0 Images fixed

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
+// next.config.ts
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  // Fixes the Turbopack warning by explicitly setting the project root.
   turbopack: {
     root: __dirname,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
   // Do NOT set basePath or assetPrefix for a user/organization site or custom domain.
 };
